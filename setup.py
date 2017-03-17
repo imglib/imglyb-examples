af3162a491bba4186c0fccde7db44c3207e812ee
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2017-03-17 17:23:17 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+import glob
+from subprocess import call
+from distutils.core import setup
+from distutils.command.build_py import build_py
+
+setup(
+    name='imglyb-examples',
+    version='0.0.1',
+    author='Philipp Hanslovsky',
+    author_email='hanslovskyp@janelia.hhmi.org',
+    description='Examples for the imglyb.',
+    packages=['imglyb-examples'],
+    install_requires=['imglyb', 'jnius', 'vigra', 'scikit-image', 'numpy']
+)

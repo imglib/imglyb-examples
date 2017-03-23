fec6776b355a1dbfa436285e5961b608832be4ea
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2017-03-23 08:12:46 -0400

    Use DistanceTransform instead of Difference of Gaussians as example
    Requires imglib2-algorithm-0.6.4-SNAPSHOT dependency

--- imglyb-examples/butterfly.py
+++ imglyb-examples/butterfly.py
@@ -23,7 +23,8 @@ if __name__ == "__main__":
 	Converters = autoclass( 'net.imglib2.converter.Converters' )
 	ARGBType = autoclass ( 'net.imglib2.type.numeric.ARGBType' )
 	RealType = autoclass ( 'net.imglib2.type.numeric.real.DoubleType' )
-	DifferenceOfGaussian = autoclass( 'net.imglib2.algorithm.dog.DifferenceOfGaussian' )
+	DistanceTransform = autoclass( 'net.imglib2.algorithm.morphology.distance.DistanceTransform' )
+	DISTANCE_TYPE = autoclass( 'net.imglib2.algorithm.morphology.distance.DistanceTransform$DISTANCE_TYPE' )
 	Views = autoclass( 'net.imglib2.view.Views' )
 	Executors = autoclass( 'java.util.concurrent.Executors' )
 	t = ARGBType()
@@ -41,23 +42,19 @@ if __name__ == "__main__":
 	gradient = vigra.filters.gaussianGradientMagnitude( avg, 5.0 )
 	gradient_conv = RealARGBConverter( gradient.min(), gradient.max() )
 
-	dog = np.zeros( avg.shape, dtype=avg.dtype )
-	print( "DoG output mean before applying ImgLib2 DoG: ", dog.mean() )
-	DifferenceOfGaussian.DoG(
-		[2.0, 2.0],
-		[5.0, 5.0],
-		Views.extendBorder( util.to_imglib( avg ) ),
-		util.to_imglib( dog ),
-		Executors.newFixedThreadPool( multiprocessing.cpu_count() * 3 )
-		)
-	print( "DoG output mean after before applying ImgLib2 DoG: ", dog.mean() )
-	dog_conv = RealARGBConverter( dog.min(), dog.max() )
+	dt = np.zeros( avg.shape, dtype=avg.dtype )
+	print( "Distance transform output mean before applying ImgLib2 dt: ", dt.mean() )
+	cpu_count = multiprocessing.cpu_count()
+	DistanceTransform.transform( Views.extendBorder( util.to_imglib( -gradient ) ), util.to_imglib( dt ), DISTANCE_TYPE.EUCLIDIAN, Executors.newFixedThreadPool( cpu_count ), cpu_count, 1e-2, 2e-2  )
+	print( "Distance transform output mean after applying ImgLib2 dt: ", dt.mean() )
+	dt_conv = RealARGBConverter( dt.min(), dt.max() )
 	
 
 	bdv = util.BdvFunctions.show( util.to_imglib_argb( argb ), "argb", util.options2D() )
 	util.BdvFunctions.show( Converters.convert( util.to_imglib( avg ), avg_conv, t ), "mean (numpy)", util.BdvOptions.addTo( bdv ) )
 	util.BdvFunctions.show( Converters.convert( util.to_imglib( gradient ), gradient_conv, t ), "gradient (vigra)", util.BdvOptions.addTo( bdv ) )
-	util.BdvFunctions.show( Converters.convert( util.to_imglib( dog ), dog_conv, t ), "DoG (ImgLib2)", util.BdvOptions.addTo( bdv ) )
+	util.BdvFunctions.show( Converters.convert( util.to_imglib( dt ), dt_conv, t ), "Distance Transform (ImgLib2)", util.BdvOptions.addTo( bdv ) )
+
 
 	# Show only one source at a time.
 	DisplayMode = autoclass( 'bdv.viewer.DisplayMode' )

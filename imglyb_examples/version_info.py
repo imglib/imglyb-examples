15b1ad957fbfab383bdd8d16528db203f66a0598
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-07-03 07:20:53 -0400

    Release imglyb-examples-0.1.0

--- imglyb_examples/version_info.py
+++ imglyb_examples/version_info.py
@@ -1,7 +1,7 @@
 __major__   = 0
 __minor__   = 1
 __patch__   = 0
-__tag__     = 'dev0'
+__tag__     = ''
 __version__ = f'{__major__}.{__minor__}.{__patch__}.{__tag__}'.strip('.')
 
 class _Version(object):
